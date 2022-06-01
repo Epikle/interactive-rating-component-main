9f55a8ca8162f701b88744c33df91b2c5284f428
Epikle <jani@qwia.net>
2022-06-01 15:41:38 +0300

    Added labels for inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,19 +17,24 @@ const Form = (props) => {
       <form onSubmit={submitHandler}>
         <ul>
           <li>
-            <input type="radio" name="rating" value="1" />
+            <input type="radio" name="rating" value="1" id="r1" />
+            <label htmlFor="r1" className='sr-only'>Rate 1</label>
           </li>
           <li>
-            <input type="radio" name="rating" value="2" />
+            <input type="radio" name="rating" value="2" id="r2" />
+            <label htmlFor="r2" className='sr-only'>Rate 2</label>
           </li>
           <li>
-            <input type="radio" name="rating" value="3" />
+            <input type="radio" name="rating" value="3" id="r3" />
+            <label htmlFor="r3" className='sr-only'>Rate 3</label>
           </li>
           <li>
-            <input type="radio" name="rating" value="4" />
+            <input type="radio" name="rating" value="4" id="r4" />
+            <label htmlFor="r4" className='sr-only'>Rate 4</label>
           </li>
           <li>
-            <input type="radio" name="rating" value="5" />
+            <input type="radio" name="rating" value="5" id="r5" />
+            <label htmlFor="r5" className='sr-only'>Rate 5</label>
           </li>
         </ul>
         <button>submit</button>
